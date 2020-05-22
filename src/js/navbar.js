422219625f6a9cdf95edc67968e9acf31b84f7b3
codewithmero <yogiforyouu@gmail.com>
2020-05-22 23:14:17 +0530

    Add a rule for stopping page scrolling while navbar is on.

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,11 +1,15 @@
+let body = document.querySelector("body");
+console.log(body);
 let menuBtn = document.querySelector("#check");
 let menuUl = document.querySelector(".nav-menu");
 
 menuBtn.addEventListener('click', (e) => {
     if (menuBtn.checked) {
         menuUl.style.left = 0;
+        body.style.overflowY = "hidden";
     }
     else {
         menuUl.style.left = -100 + "%";
+        body.style.overflowY = "auto";
     }
 });
